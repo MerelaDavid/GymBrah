18bef95705f400a03767ec5f9d48f27b9d9d65ed
Ms Aesthetics <mereladavid@gmail.com>
2018-08-25 10:50:39 +0200

    New gymBrah app

--- app/src/main/java/com/example/msaesthetics/gymbrah/LoginActivity.java
+++ app/src/main/java/com/example/msaesthetics/gymbrah/LoginActivity.java
@@ -4,12 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
